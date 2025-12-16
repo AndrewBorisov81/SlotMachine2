76217fe48ac99581d9d305057dad0aafb406d973
AndrewBorisov81 <ibobojohn81@gmail.com>
2025-12-16 20:04:53 +0200

    Update states when click button spin

--- Classes/GameField.cpp
+++ Classes/GameField.cpp
@@ -40,22 +40,18 @@ bool GameField::init()
   LayerColor * bgColor = LayerColor::create(Color4B(80, 122, 220, 255));
   this->addChild(bgColor, -30);
     
-  std::vector<int> jsonData = {
+  std::vector<int> jsonData1 = {
     0, 0, 3, 3, 2, 0, 1, 3, 3, 2,
-    2, 1, 0, 3, 0, 2, 1, 1, 3, 2,
-    2, 1, 3, 3, 2, 0, 1, 1, 1, 2,
-    3, 1, 3, 0, 2, 0, 1, 0, 2, 2,
-    0, 1, 3, 3, 2, 0, 1, 3, 3, 2,
+    2, 1, 0, 3, 0, 2, 1, 1, 3, 2
   };
+  
+  std::vector<int> jsonData2 = {
+    2, 1, 3, 0, 2, 3, 2, 3, 0, 0,
+    0, 0, 2, 3, 1, 3, 1, 0, 3, 1
+  };
+    
   // When to stop wheel cell
-  int targetCell = 35;
-
-  //create box data
-  std::vector<int> boxData;
-  for (int i = 0; i < 50; i++)
-  {
-    boxData.push_back(i);
-  }
+  int targetCell = 12;
     
   //Frame Slot Machine
   auto frameSprite = Sprite::create(Constants::FRAME_WHEEL);
@@ -65,19 +61,19 @@ bool GameField::init()
   Size frameSize = frameSprite->getContentSize();
     
   //slot machine
-  SlotMachine* slotMachine1 = new SlotMachine(jsonData);
+  SlotMachine* slotMachine1 = new SlotMachine(jsonData1);
   slotMachine1->setPosition(Vec2(0.19 * frameSize.width, 0.5 * frameSize.height));
   frameSprite->addChild(slotMachine1, 30);
   _slotMachine1 = slotMachine1;
     
   //slot machine
-  SlotMachine* slotMachine2 = new SlotMachine(jsonData);
+  SlotMachine* slotMachine2 = new SlotMachine(jsonData2);
   slotMachine2->setPosition(Vec2(0.38 * frameSize.width, 0.5 * frameSize.height));
   frameSprite->addChild(slotMachine2, 30);
   _slotMachine2 = slotMachine2;
     
   //slot machine
-  SlotMachine* slotMachine3 = new SlotMachine(jsonData);
+  SlotMachine* slotMachine3 = new SlotMachine(jsonData1);
   slotMachine3->setPosition(Vec2(0.57 * frameSize.width, 0.5 * frameSize.height));
   frameSprite->addChild(slotMachine3, 30);
   _slotMachine3 = slotMachine3;
@@ -89,36 +85,20 @@ bool GameField::init()
   spinButton->setPosition(Vec2(visibleSize.width + origin.x - 0.6 * buttonSize.width, -0.6 * buttonSize.height));
   spinButton->setPressedActionEnabled(true);
     
-  spinButton->addTouchEventListener([=](Ref* pSender, Widget::TouchEventType type){
+    spinButton->addTouchEventListener([=](Ref* pSender, Widget::TouchEventType type){
     if (type == Widget::TouchEventType::ENDED)
     {
-        if(slotMachine1->getState() == SlotMachine::State::SPIN
-           || slotMachine1->getState() == SlotMachine::State::STOP) {
-            slotMachine1->startStopMachine(targetCell);
+      slotMachine1->startStopMachine(targetCell);
+           
+      this->runAction(Sequence::create(DelayTime::create(0.2f), CallFunc::create([this]() {
+        this->_slotMachine2->startStopMachine(10);}), nullptr));
+          
+      this->runAction(Sequence::create(DelayTime::create(0.4f),   // delay in seconds
+        CallFunc::create([this]() { this->_slotMachine3->startStopMachine(17); }),
+          nullptr));
+           
         }
-        this->runAction(
-            Sequence::create(
-                DelayTime::create(0.5f),   // delay in seconds
-                CallFunc::create([this]() {
-                    this->_slotMachine2->startStopMachine(26);
-                }),
-                nullptr
-            )
-        );
-        
-        this->runAction(
-            Sequence::create(
-                DelayTime::create(0.8f),   // delay in seconds
-                CallFunc::create([this]() {
-                    this->_slotMachine3->startStopMachine(5);
-                }),
-                nullptr
-            )
-        );
-      
-      //slotMachine3->startStopMachine(5);
-    }
-  });
+    });
   this->addChild(spinButton, 40);
 
   auto move = MoveBy::create(1, Vec2(0, 1.2 * buttonSize.height));
